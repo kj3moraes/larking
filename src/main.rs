7a7fe468abef85cba15d43b8be3d131cac055db1
Keane Moraes <lordvader3002@gmail.com>
2023-08-14 03:30:09 -0400

    initial commit for main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+
+mod neural_network;
+
 fn main() {
+    let topology = vec![
+        Topology { neurons: 2 },
+        Topology { neurons: 3 },
+        Topology { neurons: 1 },
+    ];
+    let nn = NeuralNetwork::new(&topology);
+
     println!("Hello, world!");
 }
