53cd1326cf3794efb044f6bea5c2228f3d5443de
Keane Moraes <lordvader3002@gmail.com>
2023-08-14 03:29:34 -0400

    added a class for all the activation function

---
+++ libs/neural-network/src/activation.rs
@@ -0,0 +1,15 @@
+
+// ACTIVATION FUNCTIONS
+use crate::*;
+
+pub fn Sigmoid(x: f32) -> f32 {
+    1.0 / (1.0 + (-x).exp())
+}
+
+pub fn ReLU(x: f32) -> f32 {
+    if x > 0.0 {
+        x.min(1.0)
+    } else {
+        0.0
+    }
+}
