de83d87ce1b9cb3c02f06812d622c735e68784d4
Keane Moraes <lordvader3002@gmail.com>
2023-09-06 22:55:11 -0400

    simplified the chromosome package

--- libs/genetic-algorithm/src/chromosomes/mod.rs
+++ libs/genetic-algorithm/src/chromosomes/mod.rs
@@ -1,7 +1,5 @@
 use crate::*;
 
-pub mod abilities;
-
 #[derive(Clone, Debug)]
 pub struct Chromosome {
     genes: Vec<f32>
@@ -22,5 +20,32 @@ impl Chromosome {
 
     pub fn iter_mut(&mut self) -> impl Iterator<Item=&mut f32> {
         self.genes.iter_mut()
+    }
+}
+
+
+impl Index<usize> for Chromosome {
+    type Output = f32;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.genes[index]
+    }
+}
+
+
+impl FromIterator<f32> for Chromosome {
+    fn from_iter<T: IntoIterator<Item=f32>>(iter: T) -> Self {
+        Self {
+            genes: iter.into_iter().collect()
+        }
+    }
+}
+
+impl IntoIterator for Chromosome {
+    type Item = f32;
+    type IntoIter = impl Iterator<Item = f32>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.genes.into_iter()
     }
 }
