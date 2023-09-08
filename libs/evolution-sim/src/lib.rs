6cdf8b337f0e0c0e2ac64721f803e1f8137e613a
Keane Moraes <lordvader3002@gmail.com>
2023-09-08 12:14:44 -0400

    initial commit for simulation

--- libs/evolution-sim/src/lib.rs
+++ libs/evolution-sim/src/lib.rs
@@ -1,3 +1,5 @@
+use nalgebra as na;
+use rand::{ Rng, RngCore };
 
 pub struct Simulation {
     world: World,
@@ -10,22 +12,54 @@ pub struct World {
 }
 
 pub struct Animal {
-    pub position: Position,
-    pub energy: f64,
-    pub direction: f64,
-    pub speed: f64,
-    pub size: f64,
-    pub color: Color,
+    pub position: na::Point2<f32>,
+    pub rotation: na::Rotation2<f32>,
+    pub speed: f32,
 }
 
 pub struct Food {
-    pub position: Position,
-    pub energy: f64,
-    pub size: f64,
-    pub color: Color,
+    pub position: na::Point2<f32>,
 }
 
-pub Position {
-    x: f32,
-    y: f32,
+impl Simulation {
+    pub fn initialize(rng: &mut dyn RngCore) -> Self {
+        Self {
+            world: World::initialize(rng),
+            age: 0
+        }
+    }
+}
+
+
+impl World {
+    pub fn initialize(rng: &mut dyn RngCore) -> Self {
+
+        let animals = (0..40)
+                        .map(|_| Animal::random(rng))
+                        .collect();
+        let food = (0..40)
+                        .map(|_| Food::random(rng))
+                        .collect();
+        Self { animals, food }
+    }
+}
+
+
+impl Animal {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            position: na::Point2::new(rng.gen(), rng.gen()),
+            rotation: na::Rotation2::new(rng.gen()),
+            speed: 0.002
+        }
+    }
+}
+
+
+impl Food {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            position: na::Point2::new(rng.gen(), rng.gen()),
+        }
+    }
 }
