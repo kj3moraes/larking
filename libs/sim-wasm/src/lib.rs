481897b1f8cb14ffdb1242992ea50512852cb768
Keane Moraes <lordvader3002@gmail.com>
2023-09-08 13:24:34 -0400

    initial commit for the wasm simulation

--- libs/sim-wasm/src/lib.rs
+++ libs/sim-wasm/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use wasm_bindgen::prelude::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[wasm_bindgen]
+pub fn tell_em() -> String {
+    "Thus spoke St. Alia-of-the-Knife".to_string()
 }
