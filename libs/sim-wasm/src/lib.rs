640c8b8b53055b50dde6329151cc4e01f0014301
Keane Moraes <lordvader3002@gmail.com>
2023-09-08 14:30:20 -0400

    initial commit for the simulation

--- libs/sim-wasm/src/lib.rs
+++ libs/sim-wasm/src/lib.rs
@@ -1,6 +1,25 @@
 use wasm_bindgen::prelude::*;
+use lib_evo_sim as sim;
+use rand::prelude::*;
 
 #[wasm_bindgen]
 pub fn tell_em() -> String {
     "\"The Reverend Mother must combine the seductive wiles of a courtesan with the untouchable majesty of a virgin goddess".to_string()
 }
+
+#[wasm_bindgen]
+pub struct Simulation {
+    rng: ThreadRng,
+    sim: sim::Simulation,
+}
+
+#[wasm_bindgen]
+impl Simulation {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        let mut rng = rand::thread_rng();
+        let mut sim = sim::Simulation::initialize(&mut trng);
+        Self { rng, sim }
+    }
+}
+
