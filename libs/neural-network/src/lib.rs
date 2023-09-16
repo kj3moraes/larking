df445d14057d9155c9d623b9b845aa32ead319f9
Keane Moraes <lordvader3002@gmail.com>
2023-09-16 17:00:55 -0400

    moved topology out

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,15 +1,11 @@
 use rand::{ Rng, RngCore };
-use crate::{neurons::Neuron, layer::Layer};
+use crate::{layer::Layer, topology::Topology};
 
 pub mod activation;
 pub mod layer;
 mod neurons;
-// The topology of the neural network is defined by the number of neurons in each layer.
-// The user must provide a vector of Topology structs to the NeuralNetwork constructor.
-// We assume that every layer is fully connected to the previous layer.
-pub struct Topology {
-    neurons: usize,
-}
+mod topology;
+
 
 pub struct NeuralNetwork {
     layers: Vec<Layer>,
