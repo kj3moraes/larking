23d64e7578439b63f1bd360e958497c6f8f51449
Keane Moraes <lordvader3002@gmail.com>
2023-09-16 16:59:18 -0400

    changed from new to rnadom in a lot of places

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,33 +1,9 @@
-use rand::Rng;
-
-use crate::neurons::Neuron;
+use rand::{ Rng, RngCore };
+use crate::{neurons::Neuron, layer::Layer};
 
 pub mod activation;
+pub mod layer;
 mod neurons;
-
-struct Layer {
-    neurons: Vec<Neuron>,
-}
-
-
-impl Layer {
-
-    fn new(input_neurons:usize, output_neurons: usize) -> Self {
-        let neurons = (0..output_neurons)
-                    .map(|_| Neuron::new(input_neurons))
-                    .collect();
-        Self { neurons }
-    }
-
-    fn propogate(&self, inputs: Vec<f32>) -> Vec<f32> {
-        self.neurons
-            .iter()
-            .map(|neuron| neuron.propogate(&inputs))
-            .collect()
-    }
-}
-
-
 // The topology of the neural network is defined by the number of neurons in each layer.
 // The user must provide a vector of Topology structs to the NeuralNetwork constructor.
 // We assume that every layer is fully connected to the previous layer.
@@ -41,17 +17,36 @@ pub struct NeuralNetwork {
 
 impl NeuralNetwork {
 
-    pub fn new(topology: &[Topology]) -> Self {
-        assert!(topology.len() >= 2);
+    pub(crate) fn new(layers: Vec<Layer>) -> Self {
+        Self { layers }
+    }
+
+    pub fn from_weights(layers: &[Topology], weights: impl IntoIterator<Item = f32>) -> Self {
+        assert!(layers.len() > 1);
+
+        let mut weights = weights.into_iter();
+
+        let layers = layers
+            .windows(2)
+            .map(|layers| Layer::from_weights(layers[0].neurons, layers[1].neurons, &mut weights))
+            .collect();
+
+        if weights.next().is_some() {
+            panic!("got too many weights");
+        }
+
+        Self::new(layers)
+    }
+
+    pub fn random(rng: &mut dyn RngCore, topology: &[Topology]) -> Self {
+        assert!(!topology.is_empty());
 
         let layers = topology
-            .windows(2)
-            .map(|layers| {
-                Layer::new(layers[0].neurons, layers[1].neurons)
-            })
-            .collect();
-        
-        Self { layers }
+                        .windows(2)
+                        .map(|layers| Layer::random(rng, layers[0].neurons, layers[1].neurons))
+                        .collect();
+
+        Self::new(layers)
     }
 
     pub fn propogate(&self, inputs: Vec<f32>) -> Vec<f32> {
